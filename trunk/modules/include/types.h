b4c864febbf364cc269ac07de70eece331279010
tomasz.huczek <tomasz.huczek@9b5b1781-be22-0410-ac8e-a76ce1d23082>
2006-11-24 18:51:42 +0000

    zmiana w komentarzu opisujaca strukture frame_data

--- trunk/modules/include/types.h
+++ trunk/modules/include/types.h
@@ -5,13 +5,14 @@ struct frame_data
 {
 	unsigned int  width;
 	unsigned int  height;
-	/*Ilosc bitow na kanal - zazwyczaj 8*/
+
+	/** Ilosc bajtow na pixel - dla RGB 3*/
 	unsigned int  depth;
-	/*Tablica wartosci pikseli, jedno wymiarowowa o dlugosci width*height*depth
-	  Jeden piksel to 3 kolejne elementy w kolejnoœci G B R
-	  P1       |P2      |P3      .....
-	  [0][1][2][3][4][5][6][7][8]..... itd
-	  |G  B  R |G  B  R |G  B  R|
+	/** Tablica wartosci pikseli, jedno wymiarowowa o dlugosci width*height*depth
+	    Jeden piksel to 3 kolejne elementy w kolejnoœci G B R
+	    P1       |P2      |P3      .....
+	    [0][1][2][3][4][5][6][7][8]..... itd
+	    |G  B  R |G  B  R |G  B  R|
 	*/
 	unsigned char * bits;
 };
